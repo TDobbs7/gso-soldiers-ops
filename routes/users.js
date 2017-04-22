6525cc9546edd3e5000ee9768a7f4cd948ba185f
Anthony Dobson <Anthony Dobson>
2017-04-22 17:23:15 -0400

    Commit before pull

--- routes/users.js
+++ routes/users.js
@@ -12,15 +12,6 @@ router.get('/', function(req, res, next) {
     });
 });
 
-// router.get('/:email', function(req, res, next) {
-//     User.findOne({"email" : req.params.email}, function(err, user) {
-//         if (err) return next(err);
-
-//         if (!user) res.status(404).json({'message' :"User with Email " + req.params.email + " not found"});
-//         else res.json({"user" : user, "timestamp" : new Date(new Date().getTime())});
-//     });
-// });
-
 router.post('/', function(req, res, next) {
     var db = req.db;
     var user_coll = db.get('users');
