0d1f4cb86611ef712cb021f565dbc645d70b6ca0
Anthony Dobson <Anthony Dobson>
2017-05-03 23:28:56 -0400

    After merge

--- routes/med_reqs.js
+++ routes/med_reqs.js
@@ -1,20 +1,68 @@
-<div class="container" data-ng-if="user.role.class === 'Player' || user.role.class === 'Coach'">
+var express = require('express');
+var router = express.Router();
 
-	<form ng-submit="postReq(player, staff, issue)">
-		<div data-ng-if="user.role.class === 'Coach'">
-			<h3><small>Select Your Player:</small></h3>
-			<select ng-model="player">
-				<option ng-repeat="player in players" value="{{player.email}}">{{player.lname}}, {{player.fname}}: {{player.role.position}} ({{player.email}})</option>
-			</select>
-		</div>
+/* GET medical requests listing. */
+router.get('/', function(req, res, next) {
+    var db = req.db;
+    var med_coll = db.collection('medical_req');
 
-		<h3><small>Select Your Medical Staff:</small></h3>
-		<select ng-model="staff">
-			<option ng-repeat="staff in med_staff" value="{{staff.email}}">{{staff.lname}}, {{staff.fname}}: {{staff.role.position}} ({{staff.email}})</option>
-		</select>
-		<h3><small>Whats wrong?</small></h3><input type="text" ng-model="issue" size"150">
-	  	<br><br>
+    med_coll.find({}).toArray(function(err, med_reqs) {
+    	if (err) res.status(500).json({"message": "Error getting medical requests\n" + err});
+		else res.json({"medical_reqs": med_reqs});
+    });
+});
 
-	  	<input type="submit">
-	</form>
-</div>
+/*Get all medical requests done by/involving a user*/
+router.get('/:email', function(req, res, next) {
+    var db = req.db;
+    var med_coll = db.collection('medical_req');
+    var email = req.params.email;
+
+    var query = {
+        $or: [
+            {"player": email},
+            {"staff": email}
+        ]
+    };
+
+    med_coll.find(query).toArray(function(err, med_reqs) {
+    	if (err) res.status(500).json({"message": "Error getting medical requests (" + email + ")\n" + err});
+		else res.json({"medical_reqs": med_reqs});
+    });
+});
+
+router.post('/', function(req, res, next) {
+    var db = req.db;
+    var med_coll = db.collection('medical_req');
+    var med_req = req.body;
+
+    med_coll.insert(med_req, function(err, ins) {
+    	if (err) res.status(500).json({"message": "Error adding medical request (" + med_req._id + ")\n" + err});
+		else res.json({"success": true});
+    });
+});
+
+router.put('/:_id', function(req, res, next) {
+    var db = req.db;
+    var med_coll = db.collection('medical_req');
+    var new_med_req = req.body;
+
+    med_coll.update({"_id": req.params._id}, {$set: {new_med_req}}, function(err) {
+        if (err) res.status(500).json({"message": "Error updating medical request (" + med_req._id + ")\n" + err});
+        res.json({"message": "success", "data": {"timestamp" : new Date(new Date().getTime()).toUTCString()}});
+    });
+});
+
+router.delete('/:_id', function(req, res, next) {
+    var db = req.db;
+    var med_coll = db.collection('medical_req');
+    var _id = req.params._id;
+
+    med_coll.remove({"_id": _id}, function(err, del) {
+        if (err) res.status(500).json({"message": "Error deleting medical request (" + _id + ")\n" + err});
+        else res.json({"message": "success"});
+    });
+});
+
+
+module.exports = router;
